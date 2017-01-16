6b7ac3aaa385cc37aa53eac7f54d3748974eaa89
jmaturner <jmaturner@gmail.com>
2017-01-16 15:10:37 -0800

    added button
    added button for single column values.
    fixed issue"too many values to unpack"

--- theGUI.py
+++ theGUI.py
@@ -43,13 +43,24 @@ def uploaddata():
     print("data imported")
     
 
-#functions for buttons
-def plotdata(self):
+#for plotting single column data - creates time array from Fs
+def plotdata():
     style.use('ggplot')
-    t,v = np.loadtxt(rawdata,unpack=True)
-    global samples
-    samples=len(v) 
-    time = (samples/Fs)
+    v1=np.loadtxt(rawdata,unpack=True)
+    #elapsedtime=samples*1/Fs
+    samples=len(v1)
+    t1=np.linspace(0,samples-1,samples)
+    plt.plot(t1,v1)
+    plt.title('Signal Plot')
+    plt.ylabel('voltage')
+    plt.xlabel('time')
+    plt.show()
+
+#plots data that has time and voltage
+def plotdata2():
+    style.use('ggplot')
+    t=np.loadtxt(rawdata,unpack=True,usecols=(0,))
+    v=np.loadtxt(rawdata,unpack=True,usecols=(1,))  
     plt.plot(t,v)
     plt.title('Signal Plot')
     plt.ylabel('voltage')
@@ -63,6 +74,12 @@ def dofft(self):
     fft1=np.fft.fft(v)
     freqs=np.fft.fftfreq(len(fft1))
     inhertz=abs(freqs*Fs)
+    
+    v1 = np.loadtxt(rawdata,unpack=True)
+    fft1=np.fft.fft(v)
+    freqs=np.fft.fftfreq(len(fft1))
+    inhertz=abs(freqs*Fs)    
+
     plt.plot(inhertz,abs(fft1))
     plt.title('Signal Spectrum FFT')
     plt.xlabel('Freq Hz')
@@ -93,19 +110,25 @@ class StartPage(tk.Frame): #makes a new page
                             command=getFs)
         button1.pack(pady=10,padx=10)
  
-        #add button to execute function plotdata
+        #add button to plot single column data - voltage only
         button2 = tk.Button(self, text="Upload a data file",
                             command=uploaddata)
         button2.pack(pady=10,padx=10)
-        #add button to execute function plotdata
-        button3 = tk.Button(self, text="Plot the data in time domain",
-                            command=lambda: plotdata("this worked"))
+
+        #add button that handles 2 column, time, voltage data
+        button3 = tk.Button(self, text="Plot Single Column data",
+                            command=plotdata)
         button3.pack(pady=10,padx=10)
-        
+
+        #add button to execute function plotdata
+        button4 = tk.Button(self, text="Plot two column data",
+                            command=plotdata2)
+        button4.pack(pady=10,padx=10)        
+
         #add button to perform FFT on data
-        button4 = tk.Button(self, text="Perform the FFT and plot",
+        button5 = tk.Button(self, text="Perform the FFT and plot",
                             command=lambda: dofft("this worked"))
-        button4.pack(pady=10,padx=10)
+        button5.pack(pady=10,padx=10)
         
 
         
