a0b93997083e8bcd5989f7d88aa5a58896d584d7
jmaturner <jmaturner@gmail.com>
2017-01-29 15:58:15 -0800

    user enters Fc
    User can submit cutoff frequencies. These still need to mapped to
    filtering/plotting functions

--- theGUI.py
+++ theGUI.py
@@ -1,15 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 import tkinter as tk
 import numpy as np
-import csv
+from scipy import signal
 from matplotlib import pyplot as plt
 from matplotlib import style
-from tkinter import *
 import sounddevice as sd
 import soundfile as sf
 
 
 
 LARGE_FONT = ("Verdana", 12)
+
 
 class sigapp(tk.Tk):
 
@@ -41,35 +48,63 @@ class sigapp(tk.Tk):
 
 #gets the variable fs if entered
 def getFs():
-    global fs
-    fs=samp.get()         
+    global fs, nyq
+    fs=samp.get()   
+    nyq=0.5*fs      
     print("The sampling frequency in Hz is %d" % fs )
+    print("The Nyquist Frequency is %d" % nyq)
 
-#browse for path to data file
+def getFc1():
+    global fcl
+    fcl = fc1.get()
+    print(fc1.get())
+
+def getFc2():
+    global fch
+    fch = fc2.get()
+    print(fc2.get())    
+
+    #this function allows the user to browse for the data file
 def uploaddata():
     global rawdata
-    rawdata = filedialog.askopenfilename()
+    rawdata = tk.filedialog.askopenfilename()
     print("data imported")
-   
 
-#for plotting single column data - creates time array from Fs
-def plotdata():
-    style.use('ggplot')
+    
+#highpass filter - parameters will be passed from user at GUI    
+def plotfiltdata():
     v1=np.loadtxt(rawdata,unpack=True)
     samples=len(v1)
     t1=np.linspace(0,samples-1,samples)
-    plt.plot(t1,v1)
+    #scaled to accomodate any Fs
+    #e.g 0.05 represents 15 hz for 600 Hz signal
+    fc = fc1.get()
+    low=fc/nyq  #fc = cutoff
+    #6th order highpass
+    b,a=signal.butter(6,low,'high')
+    y=signal.filtfilt(b,a,v1)
+    plt.plot(t1,y,'r',t1,v1,'k')
     plt.title('Signal Plot')
     plt.ylabel('voltage')
     plt.xlabel('time')
     plt.show()
 
-#plots two column data
-def plotdata2():
+     
+#plots data that has time and voltage
+def plotdata():
     style.use('ggplot')
-    t=np.loadtxt(rawdata,unpack=True,usecols=(0,))
-    v=np.loadtxt(rawdata,unpack=True,usecols=(1,))  
-    plt.plot(t,v)
+    if CheckVar1.get() == 0:
+        print(CheckVar1.get())
+        v1=np.loadtxt(rawdata,unpack=True)
+        samples=len(v1)
+        t1=np.linspace(0,samples-1,samples)
+        plt.plot(t1,v1)
+    else:
+        print(CheckVar1.get())
+        t=np.loadtxt(rawdata,unpack=True,usecols=(0,))
+        v=np.loadtxt(rawdata,unpack=True,usecols=(1,))  
+        plt.plot(t,v)   
+        
     plt.title('Signal Plot')
     plt.ylabel('voltage')
     plt.xlabel('time')
@@ -92,7 +127,7 @@ def stop():
     except:
         sd.stop(data)
         
-#Performs fft on data
+#this function performs fft on data
 def dofft(self):
     style.use('ggplot')
     try:
@@ -111,8 +146,9 @@ def dofft(self):
         fft1=np.fft.fft(v)
         freqs=np.fft.fftfreq(len(fft1))
         inhertz=abs(freqs*fs)
+
         plt.plot(inhertz,abs(fft1))
-        plt.title('FFT')
+        plt.title('Signal Spectrum FFT')
         plt.xlabel('Freq Hz')
         plt.ylabel('Power')
         plt.show()
@@ -124,57 +160,64 @@ class StartPage(tk.Frame): #makes a new page
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Enter fs if necessary", font=LARGE_FONT)
+        label = tk.Label(self, text="Signal Processing Tool V1.0", font=LARGE_FONT)
         label.grid(row=0,column=2,columnspan=4)
 
         #add button to enter Fs sampling freq
         global samp 
-        samp=IntVar()
+        samp=tk.IntVar()
         mEntry = Entry(self, textvariable=samp).grid(row=1,column=3)     
         
-        button1 = tk.Button(self, text="Submit Sample Freq",
-                            command=getFs)
+        button1 = tk.Button(self, text="Submit Sample Freq",command=getFs)
         button1.grid(row=1, column=2)
- 
+        
+        global fc1, fc2
+        fc1 = tk.IntVar()
+        fc2 = tk.IntVar()
+        Fc1Entry = Entry(self, textvariable=fc1).grid(row=3,column=5)     
+        Fc2Entry = Entry(self,textvariable=fc2).grid(row=4,column=5)
+        buttonfc1 = tk.Button(self, text="Submit Fc1",command=getFc1)
+        buttonfc2 = tk.Button(self, text="Submit Fc2",command=getFc2)
+        buttonfc1.grid(row=3, column=4)
+        buttonfc2.grid(row=4, column=4)
+        
+        #checkbox for two-column data
+        global CheckVar1
+        CheckVar1 = IntVar()
+        C1 = tk.Checkbutton(self,text="data contains time", variable=CheckVar1,onvalue=1,offvalue=0)
+        C1.grid(row=2,column=3)      
+        
         #upload data button
-        button2 = tk.Button(self, text="Upload a data file",
-                            command=uploaddata)
-        button2.grid(row=2, column=2)
-
+        button2 = tk.Button(self, text="Upload a data file",command=uploaddata)
+        button2.grid(row=2, column=2, pady=20)
 
         #single column data
-        button3 = tk.Button(self, text="Plot Single Column data",
-                            command=plotdata)
-        button3.grid(row=4, column=2,pady=20)
-
-        #two column data
-        button5 = tk.Button(self, text="Plot two column data",
-                            command=plotdata2)
-        button5.grid(row=4, column=4)        
-        #performs fft
-        button6 = tk.Button(self, text="Plot the FFT",
-                            command=lambda: dofft("this worked"))
-        button6.grid(row=5, column=3)
-
-        #button to play the signal
-        button7 = tk.Button(self, text="Play it",
-                            command=playit)
-        button7.grid(row=7, column=3)
-        button8 = tk.Button(self, text="stop",
-                            command=stop)
-        button8.grid(row=8,column=3)
+        button3 = tk.Button(self, text="Plot time domain",command=plotdata)
+        button3.grid(row=3, column=2,pady=20)    
         
+        #performs fft
+        button6 = tk.Button(self, text="Plot FFT", command=lambda: dofft("this worked"))
+        button6.grid(row=3, column=3)
+
+        #stop and play buttons
+        button7 = tk.Button(self, text="Play", command=playit)
+        button7.grid(row=7, column=3, pady=15)
+        button8 = tk.Button(self, text="stop", command=stop)
+        button8.grid(row=7,column=2, pady=15)
+       
+        #performs bandpass filter
+#        button9 = tk.Button(self, text="LowPass", command=bandfilt)
+#        button9.grid(row=4, column=2)
+       
+        #Plot filtered data
+        button10 = tk.Button(self, text="Plot Filtered Signal", command=plotfiltdata)
+        button10.grid(row=4, column=3)        
 
         
         
 
 app = sigapp()
 app.mainloop()
-
-
-
-
-
 
 
 
